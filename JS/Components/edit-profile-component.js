d4fa1bb24e9372f1d6e8495dc6daecdc16cf5a7d
amanda <am.morang@gmail.com>
2018-06-16 09:56:32 -0400

    updated CSS & added gitignore

--- JS/Components/edit-profile-component.js
+++ JS/Components/edit-profile-component.js
@@ -9,7 +9,7 @@ const editProfile = {
     <label>Contact</label>
     <input type="text" ng-model="$ctrl.user.contactInfo">
     <label>Bio</label>
-    <textarea type="text" rows="15" cols="20" ng-model="$ctrl.user.bio"></textarea>
+    <textarea type="text" rows="5" cols="5" ng-model="$ctrl.user.bio"></textarea>
     <button class="updatebtn">Update</button>
   </form>
   `,
